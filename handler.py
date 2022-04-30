9ead61882dc236cf25b1afcbf258a17d45b40b43
MemoOR <dethkarz2009@hotmail.com>
2022-04-29 19:10:57 -0500

    commit message

---
+++ handler.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+def hello(event, context):
+    enviroment_var = os.environ['MY_VARIABLE']
+    
+    body = {
+        "message": "Go Serverless!! deployed correctly with github action",
+        "input": enviroment_var
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
---
+++ script.py
@@ -0,0 +1,6 @@
+def run_script():
+    print("\n\nHello you were able to run a python script!!\n\n")
+    return
+
+if __name__ == "__main__":
+    run_script()
