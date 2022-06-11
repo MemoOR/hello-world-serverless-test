ac1759df5ece3d8479f4a69b6dfc750c244d6889
MemoOR <dethkarz2009@hotmail.com>
2022-06-10 20:40:44 -0500

    testing sqs

--- services/first_lambda/handler.py
+++ services/first_lambda/handler.py
@@ -1,24 +1,87 @@
-import json, os, requests
+import os
+import json
+import boto3
+import random
+import string
 
-def hello(event, context):
+def genMessageDeduplicationId(chars = string.ascii_uppercase + string.digits, N=10):
+    return ''.join(random.choice(chars) for _ in range(N))
 
-    enviroment_var = os.environ['MY_VARIABLE']
-
-    try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-    except requests.RequestException as e:
-        print(e)
-        raise e
+def lambda_handler(event, context):
+    sqs = boto3.client('sqs')
     
-    body = {
-        "message": "Go Serverless!! deployed correctly with github action",
-        "input": enviroment_var
+    queue1_name = os.environ['QUEUE1_NAME']
+    queue2_name = os.environ['QUEUE2_NAME']
+    
+    groupId = os.environ['MessageGroupId']
+    
+    queue1 = sqs.get_queue_url(QueueName=queue1_name)
+    queue2 = sqs.get_queue_url(QueueName=queue2_name)
+    
+    json_dict = {
+        "op": "pago",
     }
+    
+    json_dict = json.dumps(json_dict)
+    
+    response = sqs.send_message(
+        QueueUrl=queue1['QueueUrl'],
+        MessageBody = json_dict,
+        
+        # Necesarios por ser lista fifo
+        MessageGroupId = groupId, 
+        MessageDeduplicationId = genMessageDeduplicationId()
+    )
+    
+    print(response)
+    print("Looking this Id:")
+    print(response['MessageId'])
+    
+    # genero el name del key de los Message attributes
+    id = 'id' + response['MessageId']
+    print("waiting for:")
+    print(id)
+    
+    results = sqs.receive_message(
+        QueueUrl=queue2['QueueUrl'],
+        AttributeNames=[
+            'All'
+        ],
+        MaxNumberOfMessages=10,
+        MessageAttributeNames=['All'],  
+        VisibilityTimeout=5, # importante para poder borrar el mensaje con código
+        WaitTimeSeconds=10 # tiempo que va a esperar la función a que haya algún mensaje
+    )
+    
+    if len(results) > 0:
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        "location": ip.text.replace("\n", "")
+        for i in range(len(results['Messages'])):
+            if id in results['Messages'][i]['MessageAttributes']:
+                print("found")
+                
+                message = results['Messages'][i]
+                receipt_handle = message['ReceiptHandle'] # se usa para borrar el mensaje
+                
+                # Aqui se obtiene el mensaje donde viene el id de la base de datos,
+                # guardar aqui el dato en alguna variable para que se borre de 
+                # forma eficiente el mensaje
+                
+                print("The id is:")
+                print(message['MessageAttributes'][id]['StringValue'])
+                print("the random is:")
+                print(message['MessageAttributes']['randomNumber']['StringValue'])
+
+                # Importante borrar el mensaje para que se vean los siguientes
+                # al hacer otro request
+                sqs.delete_message(
+                    QueueUrl=queue2['QueueUrl'],
+                    ReceiptHandle=receipt_handle
+                )
+                break
+    else:
+        print("no data")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('SQS request made')
     }
-
-    return response
--- services/second_lambda/handler.py
+++ services/second_lambda/handler.py
@@ -1,25 +1,59 @@
-import json, os, requests
+import os
+import json
+import boto3
+import random
+import string
 
-def hello(event, context):
+def genMessageDeduplicationId(chars = string.ascii_uppercase + string.digits, N=10):
+    return ''.join(random.choice(chars) for _ in range(N))
 
-    enviroment_var = os.environ['MY_VARIABLE']
-
-    try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-    except requests.RequestException as e:
-        # Send some context about this error to Lambda Logs
-        print(e)
-        raise e
+def lambda_handler(event, context):
+    # TODO implement
+    print(event['Records'][0]['messageId'])
+    sqs = boto3.client('sqs')
     
-    body = {
-        "message": "Go Serverless!! deployed correctly with github action",
-        "input": enviroment_var
+    queue_name = os.environ['QUEUE_NAME']
+    queue = sqs.get_queue_url(QueueName=queue_name)
+    
+    groupId = os.environ['MessageGroupId']
+    
+    # obtengo el id del mensaje recibido y genero el nombre del key para los
+    # message attributes
+    id = 'id' + event['Records'][0]['messageId']
+    
+    print("sending to:")
+    print(id)
+
+    
+    # JSON para mandar los message attributes
+    messageAttribs = {
+            id: {
+            'DataType': 'String',
+            'StringValue': event['Records'][0]['messageId']
+        }, 
+        # aqui iria el id de la base de datos
+            'randomNumber': {
+            'DataType': 'Number',
+            'StringValue': str(random.randint(0,9))
+        }
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        "location": ip.text.replace("\n", "")
+    print(messageAttribs)
+    
+    response = sqs.send_message(
+        QueueUrl=queue['QueueUrl'],
+        MessageBody = (
+            'Here is the response body'
+        ),
+        MessageAttributes = messageAttribs,
+        # Necesarios por ser lista fifo
+        MessageGroupId = groupId, 
+        MessageDeduplicationId = genMessageDeduplicationId()
+    )
+    
+    print(response)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successful request made')
     }
-
-    return response
